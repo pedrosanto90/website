3f44ad9668db018b3b7f5d40bd9793589a534033
pedrosanto90 <pedro.bb.90@gmail.com>
2025-09-04 22:26:14 +0100

    ...

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import path from "path";
 
 const app = express();
@@ -8,6 +8,16 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.post("/contact", async (req: Request, res: Response) => {
+  const { nome, email, message } = req.body;
+
+  if (!nome || !email || !message) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Preenche todos os campos." });
+  }
 });
 
 app.listen(PORT, () => {
