5d0313e3b3c175678842b473fa244c814f668762
Pedro Santo <pedro.santo@domatica.pt>
2025-12-03 23:36:01 +0000

    update

--- components/BlogPosts.tsx
+++ components/BlogPosts.tsx
@@ -1,4 +1,4 @@
-import posts from '../posts/posts';
+import posts from "../posts/posts";
 
 export default function BlogPosts() {
   const blogPosts = posts || [];
@@ -6,7 +6,9 @@ export default function BlogPosts() {
     <div className="container mx-auto px-6 py-10 flex flex-col content-left items-center">
       {blogPosts.map((post, index) => (
         <div key={index} className="mb-12">
-          <h2 className="text-3xl font-bold text-gray-900 mb-4">{post.title}</h2>
+          <h2 className="text-3xl font-bold text-gray-900 mb-4">
+            {post.title}
+          </h2>
           <p className="text-gray-700 mb-4">{post.content}</p>
           <div className="flex flex-wrap gap-2">
             {post.tags.map((tag, tagIndex) => (
@@ -18,8 +20,9 @@ export default function BlogPosts() {
               </span>
             ))}
           </div>
+          <p className="text-gray-700 mt-4">{post.date}</p>
         </div>
       ))}
     </div>
-  )
+  );
 }
--- posts/posts.ts
+++ posts/posts.ts
@@ -1,13 +1,15 @@
 const posts = [
   {
     title: "First Post",
+    tags: ["introduction", "welcome"],
+    date: "2024-06-15",
     content: "This is the content of the first post.",
-    tags: ["introduction", "welcome"]
   },
   {
     title: "Second Post",
+    tags: ["update", "news"],
+    date: "2024-06-10",
     content: "This is the content of the second post.",
-    tags: ["update", "news"]
   }
 ];
 
