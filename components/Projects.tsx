6b5eb4978c4ccd429f7d288ea5f00c342679764d
Pedro Santo <pedro.santo@domatica.pt>
2025-12-03 23:38:41 +0000

    hide projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,30 +1,31 @@
-'use client';
+"use client";
 
 import projects from "../utils/projects";
 import ProjectCard from "./ui/ProjectCard";
+
 export default function Projects() {
   const projectList = projects.projects;
   return (
-      <section id="projects" className="py-20 bg-white">
-        <div className="container mx-auto px-6">
-          <div className="max-w-6xl mx-auto">
-            <h2 className="text-4xl font-bold text-gray-900 mb-12 text-center">
-              Featured Projects
-            </h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-              {projectList.map((project, index) => (
-                  <ProjectCard
-                    key={index}
-                    name={project.name}
-                    description={project.description}
-                    link={project.link}
-                    technologies={project.technologies}
-                    image={project.image}
-                  />
-              ))}
-            </div>
+    <section id="projects" className="py-20 bg-white hidden">
+      <div className="container mx-auto px-6">
+        <div className="max-w-6xl mx-auto">
+          <h2 className="text-4xl font-bold text-gray-900 mb-12 text-center">
+            Featured Projects
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {projectList.map((project, index) => (
+              <ProjectCard
+                key={index}
+                name={project.name}
+                description={project.description}
+                link={project.link}
+                technologies={project.technologies}
+                image={project.image}
+              />
+            ))}
+          </div>
         </div>
-        </div>
-      </section>
+      </div>
+    </section>
   );
 }
